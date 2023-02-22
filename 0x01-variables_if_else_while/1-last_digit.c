f5a605b10ad3d1f4ff605139fb536d3716ac1175
Abram-K <abram.keeve@gmail.com>
2023-02-22 09:08:40 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,11 +22,11 @@ int main(void)
 	}
 	else if ((n % 10) > 5)
 	{
-		printf("bigger than 5\n");
+		printf("greater than 5\n");
 	}
 	else
 	{
-		printf("is less than 6 and not 0\n");
+		printf("less than 6 and not 0\n");
 	}
 	return (0);
 }
