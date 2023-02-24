09a7696020eef5c8a0c6f182b9d0fbc917828685
Abram-K <abram.keeve@gmail.com>
2023-02-23 23:17:33 -0800

    Maybe

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Iterate 0 - 9 using only putchar
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+
+
+
+
+	return (0);
+}
