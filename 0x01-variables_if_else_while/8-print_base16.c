1ade7f0a4022ba3212aac92f2db9992241f05baf
Abram-K <abram.keeve@gmail.com>
2023-02-23 23:44:38 -0800

    Based 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Iterate 0 - 9 using only putchar
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		putchar(48 + num);
+	}
+
+	for (num = 'a' ; num <= 'f' ; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+
+
+
+
+
+	return (0);
+}
